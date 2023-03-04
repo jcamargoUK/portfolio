87a78a5dfd07829014d78e35fef35e9babc9b122
Julio Camargo <jcamargo@hotmail.co.uk>
2023-03-04 16:34:29 +0000

    added footer links

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -1,5 +1,28 @@
-export default function Footer(){
-  return(
-    <h1>Footer</h1>
+import styles from '@/styles/Footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Footer() {
+  return (
+    <div className={styles.main}>
+      <Link href='https://github.com/jcamargoUK'>
+        <Image
+          src='/assets/github.png'
+          width={30}
+          height={30}
+          alt='gitHub link logo'
+        />
+      </Link>
+      <Link href='https://www.linkedin.com/in/julio-camargo-developer/'>
+        <Image
+          src='/assets/linkedin.png'
+          width={30}
+          height={30}
+          alt='linkedin link logo'
+        />
+      </Link>
+
+    </div>
   )
 }

9f3b6b86d9dae670801d53dcf772d84720da7b71
Julio Camargo <jcamargo@hotmail.co.uk>
2023-03-04 12:33:03 +0000

    navbar working

--- components/Layout/NavBar/NavBar.js
+++ components/Layout/NavBar/NavBar.js
@@ -1,12 +1,27 @@
 import Link from "next/link";
 import styles from "@/styles/NavBar.module.css";
 import ToggleSwitch from "./Toggle_Switch";
+import React, { useState } from "react";
+
+
 
 export default function NavBar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+const handleToggle = () => {
+  setIsOpen(!isOpen);
+};
+
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1 className={styles.h1}>JC</h1>
-      <ul>
+      <button
+        className={styles.toggle}
+        onClick={handleToggle}
+      >
+        <span className={styles.icon}>&#9776;</span>
+      </button>
+      <ul className={`${styles.links} ${isOpen ? styles.open : ''}`}>
         <li>
           <Link href="/">Home</Link>
         </li>

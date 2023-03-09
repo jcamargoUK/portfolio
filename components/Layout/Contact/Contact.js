84255084e898d0ed9b04721eec3496d83b789ade
Julio Camargo <jcamargo@hotmail.co.uk>
2023-03-09 12:37:58 +0000

    ading contact me form

---
+++ components/Layout/Contact/Contact.js
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fr94leh', 'template_u3ohpes', form.current, 'M2ce014ipX35qbMwx')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
+import { ContactUs } from "../../components/Layout/Contact/Contact"
+
 export default function Contact(){
   return(
-    <h1>Contact Me</h1>
+    <ContactUs />
   )
 }

4284f2d1653a5aed0af88ee0ef5c948fa9694252
Julio Camargo <jcamargo@hotmail.co.uk>
2023-03-28 11:08:53 +0100

    adding projects to the home page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,13 +1,26 @@
 // import styles from '@/styles/Portfolio.module.css'
 import Getting_In_Touch_Button from '../../components/Layout/Getting_In_Touch_Button/GettingInTouch'
 import ArrowDown from '../../components/Layout/Arrow_Container/ArrowDown'
+import styles from '@/styles/Portfolio.module.css'
+// import CanvaPresentation from '../../components/Layout/Canva/homegrowPresentation'
 
-export default function Portifolio(){
-  return(
-    <>
-    <h1>Portfolio</h1>
-    <Getting_In_Touch_Button />
-    <ArrowDown />
-    </>
+export default function Portifolio() {
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.title}>Some of my recently work</h1>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <iframe className={styles.iframe} loading="lazy"
+            src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFYySVxGA0&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+          </iframe>
+        </div>
+        <div className={styles.card}>Two</div>
+        <div className={styles.card}>Three</div>
+        <div className={styles.card}>Four</div>
+      </div>
+      <Getting_In_Touch_Button />
+      <ArrowDown />
+    </div>
   )
 }
+

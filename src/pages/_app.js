9f78ab06fb701af2f229169ee8b5e486b9227b21
Julio Camargo <jcamargo@hotmail.co.uk>
2023-04-23 16:41:21 +0100

    background for the main page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,21 @@
 import '@/styles/globals.css'
 import Layout from '../../components/Layout/Layout/Layout'
+import { useEffect } from 'react'
+
 
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const Pagescript = document.createElement('script')
+    Pagescript.setAttribute("id", Pagescript)
+    Pagescript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js")
+    document.getElementsByTagName('head')[0].appendChild(Pagescript)
+    return() => {
+      if(Pagescript){
+        Pagescript.remove()
+      }
+      }
+  }, [])
   return(
   <Layout>
   <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,44 @@
-// import Head from 'next/head'
+import Head from 'next/head'
+import RINGS from "vanta/dist/vanta.rings.min";
+import * as THREE from 'three'
 import styles from '@/styles/Home.module.css'
 import Getting_In_Touch_Button from '../../components/Layout/Getting_In_Touch_Button/GettingInTouch'
-import ArrowDown from '../../components/Layout/Arrow_Container/ArrowDown'
-import Image from 'next/legacy/image'
+import { useEffect, useRef, useState } from 'react'
 
 
 export default function Home() {
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const vantaRef = useRef(null)
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(RINGS({
+        el: vantaRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 520.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x7f585,
+        points: 7.00,
+        maxDistance: 24.00,
+        spacing: 18.00,
+        backgroundColor: 0x0,
+        THREE
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
 
   return (
-    <div className={styles.main}>
+    <div className={styles.container}>
+      <Head>
+        <title>Julio Cezar</title>
+      </Head>
+    <div className={styles.main} ref={vantaRef}>
       <div className={styles.container}>
         <div className={styles.welcomeMessage}>
           <h1><span classname={styles.flip}>Hi. </span> Im <span className={styles.nameSpan1}>J</span><span className={styles.nameSpan2}>u</span><span className={styles.nameSpan3}>l</span><span className={styles.nameSpan4}>i</span><span className={styles.nameSpan5}>o</span>, <br className={styles.breakLine} />
@@ -26,6 +56,7 @@ export default function Home() {
       </div>
       <Getting_In_Touch_Button />
       {/* <ArrowDown /> */}
+    </div>
     </div>
   )
 }
